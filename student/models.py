c4299b2a925b1c767ddc0b280fb3923cc67a1d3f
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-12 20:10:59 +0600

    Update models.py

--- student/models.py
+++ student/models.py
@@ -1,7 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+from django.db import transaction
+# End User Signup Model
+ 
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email,and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        try:
+            with transaction.atomic():
+                user = self.model(email=email, **extra_fields)
+                user.set_password(password)
+                user.save(using=self._db)
+                return user
+        except:
+            raise
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(email, password=password, **extra_fields)
+
+
+
+
+class EndUser(AbstractBaseUser):
+    textId               = models.CharField(max_length=150, unique = True, blank=True, null=True)
+    firstName            = models.CharField(max_length=150)
+    lastName             = models.CharField(max_length=150, blank=True, null=True)
+    email                = models.EmailField(max_length=150, unique=True, blank=True, null=True)
+    phone                = models.CharField(max_length=30, unique=True)
+    city                 = models.CharField(max_length=100)
+    country_data = (("BD", "Bangladesh"), ("IND", "India"), ("PAK","Pakistan"), ("US", "United States"), ("QTR","Qatar"))
+    country              = models.CharField(max_length=20, choices=country_data, default="BD")
+    password             = models.CharField(max_length=150)
+    otpCode              = models.CharField(max_length=6, blank=True, null=True)
+    created              = models.DateTimeField(auto_now_add=True)
+    updated              = models.DateTimeField(auto_now=False, blank=True, null=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["phone"]
+   
+    def __str__(self):
+        return self.firstName
+    
+    class Meta:
+        db_table = 'EndUser'
 
 # Create your models here.
-class Admin(models.Model):
+
+
+class AdminHod(models.Model):
     firstName  = models.CharField(max_length=255)
     lastName   = models.CharField(max_length=255, blank=True, null=True)
     email      = models.EmailField(max_length=255)
@@ -18,8 +78,9 @@ class Admin(models.Model):
         return self.firstName  
 
     
-class Staff(models.Model):
-    name  = models.CharField(max_length=255)
+class Teacher(models.Model):
+    firstName  = models.CharField(max_length=255)
+    lastName   = models.CharField(max_length=255)
     email      = models.EmailField(max_length=255)
     phone      = models.CharField(max_length=255)
     password   = models.CharField(max_length=255)
@@ -30,7 +91,7 @@ class Staff(models.Model):
     updated    = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     class Meta:
-        db_table = 'staff_info'
+        db_table = 'teacher_info'
         ordering = ['-created']
         
     def __str__(self):
@@ -51,7 +112,7 @@ class Department(models.Model):
 class Subject(models.Model):
     subject_name  = models.CharField(max_length=255, blank=True, null=True)
     department_id = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
-    staff_id      = models.ForeignKey(Staff, on_delete=models.DO_NOTHING)
+    teacher_id      = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
     created       = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated       = models.DateTimeField(auto_now=True, blank=True, null=True)
 
@@ -62,7 +123,7 @@ class Subject(models.Model):
     def __str__(self):
         return self.subject_name
     
-class Student(models.Model):
+class StudentInfo(models.Model):
     firstName  = models.CharField(max_length=255)
     lastName   = models.CharField(max_length=255, blank=True, null=True)
     email      = models.EmailField(max_length=255)
@@ -70,6 +131,8 @@ class Student(models.Model):
     password   = models.CharField(max_length=255)
     gender     = models.CharField(max_length=255)
     department  = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+    session_start_year = models.DateField()
+    session_end_year = models.DateField()
     address    = models.CharField(max_length=255, blank=True, null=True)
     profile_pic = models.ImageField(upload_to='images/', blank=True, null=True) 
     created    = models.DateTimeField(auto_now_add=True, blank=True, null=True)
@@ -95,7 +158,7 @@ class Attendance (models.Model):
         return self.subject_id
     
 class AttendanceReport(models.Model):
-    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(StudentInfo, on_delete=models.DO_NOTHING)
     attendance_id = models.ForeignKey(Attendance, on_delete=models.DO_NOTHING)
     status = models.BooleanField(default=False)
     created    = models.DateTimeField(auto_now_add=True, blank=True, null=True)
@@ -108,7 +171,7 @@ class AttendanceReport(models.Model):
         return self.student_id
     
 class LeaveReportStudent(models.Model):
-    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(StudentInfo, on_delete=models.DO_NOTHING)
     leave_date = models.DateField()
     leave_message  = models.TextField(blank=True, null=True)
     leave_status = models.BooleanField(default=False)
@@ -121,22 +184,22 @@ class LeaveReportStudent(models.Model):
     def __str__(self):
         return self.student_id
 
-class LeaveReportStaff(models.Model):
-    staff_id = models.ForeignKey(Staff, on_delete=models.DO_NOTHING)
+class LeaveReportTeacher(models.Model):
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
     leave_date = models.DateField()
     leave_message  = models.TextField(blank=True, null=True)
     leave_status = models.BooleanField(default=False)
     created    = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated    = models.DateTimeField(auto_now=True, blank=True, null=True)
     class Meta:
-        db_table = 'leaveReport_staff'
+        db_table = 'leaveReport_teacher'
         ordering = ['-created']
         
     def __str__(self):
-        return self.staff_id
+        return self.teacher_id
     
 class FeedbackStudent(models.Model):
-    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(StudentInfo, on_delete=models.DO_NOTHING)
     feedback = models.TextField()
     feedback_reply  = models.TextField(blank=True, null=True)
     created    = models.DateTimeField(auto_now_add=True, blank=True, null=True)
@@ -148,21 +211,21 @@ class FeedbackStudent(models.Model):
     def __str__(self):
         return self.feedback
     
-class FeedbackStaff(models.Model):
-    staff_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+class FeedbackTeacher(models.Model):
+    teacher_id = models.ForeignKey(StudentInfo, on_delete=models.DO_NOTHING)
     feedback = models.TextField()
     feedback_reply  = models.TextField(blank=True, null=True)
     created    = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated    = models.DateTimeField(auto_now=True, blank=True, null=True)
     class Meta:
-        db_table = 'feedback_staff'
+        db_table = 'feedback_teacher'
         ordering = ['-created']
         
     def __str__(self):
         return self.feedback
     
 class NotificationStudent(models.Model):
-    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(StudentInfo, on_delete=models.DO_NOTHING)
     message = models.TextField(blank=True, null=True)
     created    = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated    = models.DateTimeField(auto_now=True, blank=True, null=True)
@@ -173,13 +236,13 @@ class NotificationStudent(models.Model):
     def __str__(self):
         return self.feedback
     
-class NotificationStaff(models.Model):
-    staff_id = models.ForeignKey(Staff, on_delete=models.DO_NOTHING)
+class NotificationTeacher(models.Model):
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
     message = models.TextField(blank=True, null=True)
     created    = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated    = models.DateTimeField(auto_now=True, blank=True, null=True)
     class Meta:
-        db_table = 'notification_staff'
+        db_table = 'notification_teacher'
         ordering = ['-created']
         
     def __str__(self):
