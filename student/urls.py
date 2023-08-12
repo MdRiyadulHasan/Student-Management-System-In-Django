0db9a5f87ae41af1bdc6fc23a8060420b84b37dd
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-12 20:10:08 +0600

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -2,5 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('abc/', views.homeView),
+    # path('home/', views.homeView),
+    path('home/', views.homeView),
+    path('addStaff/', views.addStaff, name = "addStaff"),
+    path('home1/', views.home1View),
+    path('', views.loginPage),
+    path('registration/', views.registrationView, name = "registration"),
+    path('doLogin', views.doLogin),
 ]
