bb9ca5cf955deb2bacdc9b265d63c93d58c6bc1c
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-12 20:11:09 +0600

    Update urls.py

--- studentManagement/urls.py
+++ studentManagement/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("student.urls")),
+    path('', include("student.urls")),
+    path('adminApp/', include("adminApp.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
