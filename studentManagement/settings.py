319d63642338144dd4cdde89c168610559e2fb85
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-17 00:12:32 +0600

    Update settings.py

--- studentManagement/settings.py
+++ studentManagement/settings.py
@@ -131,12 +131,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
-# Media URL: The URL where media files will be served from
 MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-# Media Root: The absolute filesystem path to the directory where media files will be stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
