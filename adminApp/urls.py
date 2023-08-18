ec4fe2749043b231ad1b5ddc33cc47bb5f54f3bd
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-18 19:39:27 +0600

    Update urls.py

--- adminApp/urls.py
+++ adminApp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('adminhome/', views.adminHomeView),
     path('addTeacher/', views.addTeacher.as_view(), name = "addTeacher"),
     path('manageTeacher/', views.manageTeacher, name = "manageTeacher"),
+    path('updateData/<int:id>/', views.updateData, name = "deleteData"),
+    path('deleteData/<int:id>/', views.deleteData, name = "deleteData"),
  
 ]
